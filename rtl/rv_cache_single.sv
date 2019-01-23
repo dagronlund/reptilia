cb53bb2c2385ca415cfae9af8cb5a1a06ee321dc
David Gronlund <dagronlund@gmail.com>
2019-01-23 01:55:51 -0500

    Add single-cycle cache boiler plate

--- rtl/rv_cache_single.sv
+++ rtl/rv_cache_single.sv
@@ -14,7 +14,7 @@ module rv_cache_single #(
     parameter CACHE_WRITE_PROPAGATE = 0, // Cache writes expect a result
     parameter ASSOCIATIVITY = 1, // Direct Associative
     parameter BLOCK_BITS = 6, // 64 byte cache block/row
-    parameter INDEX_BITS = 4, // 
+    parameter INDEX_BITS = 4 // 
 )(
     input logic clk, rst,
     rv_mem_intf.in command, // Inbound Commands
@@ -41,5 +41,40 @@ module rv_cache_single #(
     localparam TAG_BITS = ADDR_WIDTH - BLOCK_BITS - INDEX_BITS;
 
     `STATIC_ASSERT(TAG_BITS >= 0)
+
+    logic enable, command_block, result_block, 
+            cache_command_block, cache_result_block;
+    rv_seq_flow_controller #(
+        .NUM_INPUTS(2),
+        .NUM_OUTPUTS(2)
+    ) flow_controller_inst (
+        .clk, .rst, .enable(enable),
+        .inputs_valid({command.valid, cache_result.valid}), 
+        .inputs_ready({command.ready, cache_result.ready}),
+        .inputs_block({command_block, cache_result_block}),
+
+        .outputs_valid({result.valid, cache_command.valid}),
+        .outputs_ready({result.ready, cache_command.ready}),
+        .outputs_block({result_block, cache_command_block})
+    );
+
+    // rv_memory_double_port #(
+    //     .DATA_WIDTH(DATA_WIDTH),
+    //     .ADDR_WIDTH(ADDR_WIDTH)
+    // ) rv_memory_double_port_inst (
+    //     .clk, .rst,
+
+    //     .enable0(!rst && enable0),
+    //     .write_enable0(command0.op == RV_MEM_WRITE),
+    //     .addr_in0(command0.addr),
+    //     .data_in0(command0.data),
+    //     .data_out0(result0.data),
+
+    //     .enable1(!rst && enable1),
+    //     .write_enable1(command1.op == RV_MEM_WRITE),
+    //     .addr_in1(command1.addr),
+    //     .data_in1(command1.data),
+    //     .data_out1(result1.data)
+    // );
 
 endmodule
