d7acea74ed37e25c17f984d7e1b09d4d901393d2
Alex Gotsis <agotsis@andrew.cmu.edu>
2019-01-27 17:08:43 -0500

    Add generated rocket-chip source

---
+++ rocket/generated-src/freechips.rocketchip.system.DefaultConfig/firrtl_black_box_resource_files.f
@@ -0,0 +1,4 @@
+/home/agotsis/rocket-chip/vsim/generated-src/freechips.rocketchip.system.DefaultConfig/plusarg_reader.v
+/home/agotsis/rocket-chip/vsim/generated-src/freechips.rocketchip.system.DefaultConfig/AsyncResetReg.v
+/home/agotsis/rocket-chip/vsim/generated-src/freechips.rocketchip.system.DefaultConfig/SimDTM.v
+/home/agotsis/rocket-chip/vsim/generated-src/freechips.rocketchip.system.DefaultConfig/SimDTM.cc
