3e94a69afd7d7f6db91b464ab271a3a3ba09a0de
David Gronlund <dagronlund@gmail.com>
2019-03-12 17:58:01 -0400

    :sparkles: Add signed register type

--- lib/isa/rv32.svh
+++ lib/isa/rv32.svh
@@ -14,6 +14,7 @@ package rv32;
     typedef logic [6:0] rv32_opcode_t;
     typedef logic [4:0] rv32_reg_addr_t;
     typedef logic [31:0] rv32_reg_value_t;
+    typedef logic signed [31:0] rv32_reg_signed_t;
     typedef logic [2:0] rv32_funct3_t;
     typedef logic [6:0] rv32_funct7_t;
     typedef logic [11:0] rv32_funct12_t;
