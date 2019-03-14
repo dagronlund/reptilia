1efac73a972289229c2bf9c26aa1700a42e0ae6b
David Gronlund <dagronlund@gmail.com>
2019-03-13 22:49:22 -0400

    :construction::recycle: Modifying internal signals

--- lib/gecko/gecko.svh
+++ lib/gecko/gecko.svh
@@ -12,10 +12,13 @@ package gecko;
     import rv32i::*;
 
     typedef rv32_reg_value_t gecko_pc_t;
-    typedef logic gecko_jump_flag_t;
 
     typedef logic [1:0] gecko_byte_offset_t;
     typedef logic [3:0] gecko_store_mask_t;
+
+    // Configurable Types
+    typedef logic gecko_jump_flag_t;
+    typedef logic [3:0] gecko_speculative_count_t;
 
     typedef struct packed {
         rv32_reg_value_t rs1_value;
@@ -26,6 +29,7 @@ package gecko;
     typedef struct packed {
         rv32_reg_value_t rd_value;
         rv32_reg_addr_t rd_addr;
+        gecko_jump_flag_t jump_flag;
     } gecko_reg_result_t;
 
     typedef logic [4:0] gecko_shift_amount_t;
@@ -53,13 +57,13 @@ package gecko;
     } gecko_shift_command_t;
 
     typedef enum logic {
-        GECKO_MATH_COMMAND_NORMAL = 'h0,
-        GECKO_MATH_COMMAND_ALTERNATE = 'h1
-    } gecko_math_command_alternate_t;
+        GECKO_NORMAL = 'h0,
+        GECKO_ALTERNATE = 'h1
+    } gecko_alternate_t;
 
     typedef struct packed {
-        rv32i_funct_ir_t op;
-        gecko_math_command_alternate_t alt;
+        rv32i_funct3_ir_t op;
+        gecko_alternate_t alt;
     } gecko_math_command_t;
 
     typedef struct packed {
@@ -67,9 +71,11 @@ package gecko;
         rv32_reg_value_t or_result;
         rv32_reg_value_t and_result;
         rv32_reg_value_t xor_result;
-        logic result_eq;
-        logic result_lt;
-        logic result_ltu;
+        rv32_reg_value_t lshift_result;
+        rv32_reg_value_t rshift_result;
+        logic eq;
+        logic lt;
+        logic ltu;
     } gecko_math_result_t;
 
     typedef struct packed {
@@ -82,23 +88,73 @@ package gecko;
         gecko_store_mask_t mask;
     } gecko_store_result_t;
 
+    /*************************************************************************
+     * Internal Gecko Stream Datatypes                                       *
+     *************************************************************************/
+
+    typedef struct packed {
+        rv32_reg_value_t value;
+        rv32_reg_addr_t addr;
+        logic speculative;
+    } gecko_operation_t;
+
     typedef struct packed {
         logic absolute_jump;
         rv32_reg_value_t absolute_addr;
-        logic relative_jump;
         rv32_reg_value_t relative_addr;
     } gecko_jump_command_t;
 
     typedef struct packed {
-        gecko_pc_t pc;
-        gecko_jump_flag_t jump_flag;
-    } gecko_pc_command_t;
+        logic branch;
+        rv32_reg_value_t relative_addr;
+    } gecko_branch_command_t;
 
     typedef struct packed {
-        gecko_reg_command_t reg_cmd;
-        gecko_math_command_t math_cmd;
+        logic branch;
+    } gecko_branch_signal_t;
+
+    typedef struct packed {
+        gecko_pc_t pc;
         gecko_jump_flag_t jump_flag;
-    } gecko_alu_command_t;
+    } gecko_instruction_operation_t;
+
+    typedef struct packed {
+        rv32_reg_addr_t addr;
+        rv32i_funct3_ls_t op;
+        gecko_byte_offset_t offset;
+    } gecko_mem_operation_t;
+
+    typedef struct packed {
+        rv32_reg_addr_t imm_value;
+        rv32_reg_value_t rs1_value;
+        rv32_reg_addr_t rd_addr;
+        rv32i_funct3_sys_t sys_op;
+        rv32_funct12_t csr;
+    } gecko_system_operation_t;
+
+    typedef enum logic [1:0] {
+        GECKO_EXECUTE_TYPE_EXECUTE = 2'b00,
+        GECKO_EXECUTE_TYPE_LOAD = 2'b01,
+        GECKO_EXECUTE_TYPE_STORE = 2'b10,
+        GECKO_EXECUTE_TYPE_BRANCH = 2'b11
+    } gecko_execute_type_t;
+
+    typedef struct packed {
+        gecko_execute_type_t op_type;
+        rv32i_funct3_t op;
+        gecko_alternate_t alu_alternate;
+        rv32_reg_addr_t reg_addr;
+
+        logic reuse_rs1, reuse_rs2, reuse_mem;
+        rv32_reg_value_t rs1_value, rs2_value, mem_value;
+        rv32_reg_value_t immediate_value;
+
+        logic speculative;
+    } gecko_execute_operation_t;
+
+    /*************************************************************************
+     * Internal Gecko Helper Functions                                       *
+     *************************************************************************/
 
     // Adds or subtracts with a carry bit
     function automatic gecko_add_sub_result_t gecko_add_sub(
@@ -115,43 +171,29 @@ package gecko;
 
     // Performs all ALU operations except for shifting
     function automatic gecko_math_result_t gecko_get_full_math_result(
-        input gecko_math_command_t math_cmd,
-        input gecko_reg_command_t reg_cmd
+        input rv32_reg_value_t a, b,
+        input gecko_alternate_t alt
     );
+        rv32_reg_signed_t a_signed = a;
         gecko_math_result_t math_result;
-        gecko_add_sub_result_t add_sub_result = gecko_add_sub(
-                reg_cmd.rs1_value, 
-                reg_cmd.rs2_value,
-                math_cmd.alt == GECKO_MATH_COMMAND_ALTERNATE
-        );
+        gecko_add_sub_result_t add_sub_result = gecko_add_sub(a, b, alt == GECKO_ALTERNATE);
 
-        math_result.or_result = reg_cmd.rs1_value | reg_cmd.rs2_value;
-        math_result.and_result = reg_cmd.rs1_value & reg_cmd.rs2_value;
-        math_result.xor_result = reg_cmd.rs1_value ^ reg_cmd.rs2_value;
+        math_result.or_result = a | b;
+        math_result.and_result = a & b;
+        math_result.xor_result = a ^ b;
 
-        math_result.result_eq = !(|math_result.xor_result);
-        math_result.result_lt = add_sub_result.sum[31];
-        math_result.result_ltu = add_sub_result.carry;
+        math_result.eq = !(|math_result.xor_result);
+        math_result.lt = add_sub_result.sum[31];
+        math_result.ltu = add_sub_result.carry;
 
         math_result.add_sub_result = add_sub_result.sum;
 
-        return math_result;
-    endfunction
+        // TODO: Test if shift helpers are better
+        math_result.lshift_result = a << b[4:0];
+        math_result.rshift_result = (alt == GECKO_ALTERNATE) ? 
+                (a_signed >>> b[4:0]) : (a >> b[4:0]);
 
-    // Selects which ALU operation to use from the full math operation
-    function automatic rv32_reg_value_t gecko_get_final_math_result(
-        input gecko_math_command_t math_cmd,
-        input gecko_math_result_t math_result
-    );
-        case (math_cmd.op)
-        RV32I_FUNCT3_IR_ADD_SUB: return math_result.add_sub_result;
-        RV32I_FUNCT3_IR_SLT: return math_result.result_lt ? 32'b1 : 32'b0;
-        RV32I_FUNCT3_IR_SLTU: return math_result.result_ltu ? 32'b1 : 32'b0;
-        RV32I_FUNCT3_IR_XOR: return math_result.xor_result;
-        RV32I_FUNCT3_IR_OR: return math_result.or_result;
-        RV32I_FUNCT3_IR_AND: return math_result.and_result;
-        default: return math_result.add_sub_result;
-        endcase
+        return math_result;
     endfunction
 
     function automatic gecko_store_result_t gecko_get_store_result(
@@ -197,6 +239,10 @@ package gecko;
         default: return value; // RV32I_FUNCT3_LS_W
         endcase
     endfunction
+
+    /*************************************************************************
+     * Gecko Shift Helper Functions                                          *
+     *************************************************************************/    
 
     function automatic rv32_reg_value_t gecko_reverse_bits(
         input rv32_reg_value_t value
