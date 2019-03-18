acbed553b90e13bf3c666e95609c70cb9b6ef6be
David Gronlund <dagronlund@gmail.com>
2019-03-18 02:30:19 -0400

    :fire: Remove rocket files

--- rocket/generated-src/freechips.rocketchip.system.DefaultConfig/firrtl_black_box_resource_files.f
+++
@@ -1,4 +0,0 @@
-/home/agotsis/rocket-chip/vsim/generated-src/freechips.rocketchip.system.DefaultConfig/plusarg_reader.v
-/home/agotsis/rocket-chip/vsim/generated-src/freechips.rocketchip.system.DefaultConfig/AsyncResetReg.v
-/home/agotsis/rocket-chip/vsim/generated-src/freechips.rocketchip.system.DefaultConfig/SimDTM.v
-/home/agotsis/rocket-chip/vsim/generated-src/freechips.rocketchip.system.DefaultConfig/SimDTM.cc
