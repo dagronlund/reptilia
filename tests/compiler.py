a6d3beeb845b28fd0fbc18d734f975340bdc8af0
David Gronlund <dagronlund@gmail.com>
2019-03-18 22:54:52 -0400

    :construction: Add compiler/assembler scripts

---
+++ tests/compiler.py
@@ -0,0 +1,22 @@
+import sys
+from subprocess import call
+
+base_name = sys.argv[1]
+
+name_c = base_name + ".c"
+name_o = base_name + ".o"
+name_bin = base_name + ".bin"
+name_mem = base_name + ".mem"
+
+call(["C:/gnu_riscv/bin/riscv-none-embed-gcc.exe", 
+        "-lgcc", 
+        "-nostdlib", 
+        "-nostartfiles", 
+        "-Wl,--no-relax", 
+        "-T", "linker_script.lds", 
+        "-march=rv32i", 
+        "-mabi=ilp32", 
+        name_c, "crt0.s", "libmem.c", 
+        "-o", name_o]) # Compiler
+call(["C:/gnu_riscv/bin/riscv-none-embed-objcopy.exe", "-O", "binary", name_o, name_bin]) # Symbol Removal
+call(["python", "hex_converter.py", name_bin, name_mem]) # Hex Conversion
