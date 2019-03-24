a1b53242981f555ce9b0cbcb7a2e66e635ca9522
David Gronlund <dagronlund@gmail.com>
2019-03-24 01:46:18 -0400

    :sparkles: Support arbitrary instruction/memory latency

--- rtl/gecko/gecko_micro.sv
+++ rtl/gecko/gecko_micro.sv
@@ -15,6 +15,8 @@ module gecko_micro
     import rv32i::*;
     import gecko::*;
 #(
+    parameter INST_LATENCY = 2,
+    parameter DATA_LATENCY = 2,
     parameter gecko_pc_t START_ADDR = 'h0
 )(
     input logic clk, rst,
@@ -22,10 +24,31 @@ module gecko_micro
     output logic faulted_flag, finished_flag
 );
 
+    `STATIC_ASSERT(INST_LATENCY > 0)
+    `STATIC_ASSERT(DATA_LATENCY > 0)
+
     std_mem_intf #(.DATA_WIDTH(32), .ADDR_WIDTH(32)) inst_request (.clk, .rst);
     std_mem_intf #(.DATA_WIDTH(32), .ADDR_WIDTH(32)) inst_result (.clk, .rst);
+    std_mem_intf #(.DATA_WIDTH(32), .ADDR_WIDTH(32)) inst_result_registered (.clk, .rst);
     std_mem_intf #(.DATA_WIDTH(32), .ADDR_WIDTH(32)) data_request (.clk, .rst);
     std_mem_intf #(.DATA_WIDTH(32), .ADDR_WIDTH(32)) data_result (.clk, .rst);
+    std_mem_intf #(.DATA_WIDTH(32), .ADDR_WIDTH(32)) data_result_registered (.clk, .rst);
+
+    std_mem_stage #(
+        .LATENCY(INST_LATENCY - 1)
+    ) inst_register_stage (
+        .clk, .rst,
+        .data_in(inst_result),
+        .data_out(inst_result_registered)
+    );
+
+    std_mem_stage #(
+        .LATENCY(DATA_LATENCY - 1)
+    ) data_register_stage (
+        .clk, .rst,
+        .data_in(data_result),
+        .data_out(data_result_registered)
+    );
 
     std_mem_double #(
         .MANUAL_ADDR_WIDTH(16),
@@ -38,13 +61,13 @@ module gecko_micro
     );
 
     gecko_core #(
-        .INST_LATENCY(1),
-        .DATA_LATENCY(1),
+        .INST_LATENCY(INST_LATENCY),
+        .DATA_LATENCY(DATA_LATENCY),
         .START_ADDR(START_ADDR)
     ) gecko_core_inst (
         .clk, .rst,
-        .inst_request, .inst_result,
-        .data_request, .data_result,
+        .inst_request, .inst_result(inst_result_registered),
+        .data_request, .data_result(data_result_registered),
         .faulted_flag, .finished_flag
     );
 
