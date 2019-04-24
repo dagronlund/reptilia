476c04cf719bca09d0f21005798ad3507898cc6b
Alex Gotsis <agotsis@andrew.cmu.edu>
2019-04-23 21:10:30 -0400

    Update basic vector test

--- tests/basicvec.c
+++ tests/basicvec.c
@@ -7,8 +7,8 @@ int main(int argc, char** argv) {
   int test1[VLEN] = {1,1,1,1};
   int test2[VLEN] = {1,2,3,4};
   int x = 2;
+  int y = 986;
   int z = 9;
 
-  vsetvl(x, z);
-  vfence();
+  vsetvl(x, y, z);
 }
--- tests/basicvec.s
+++ tests/basicvec.s
@@ -20,7 +20,8 @@
 	.type	main, @function
 main:
 	addi	sp,sp,-80
-	sw	s0,76(sp)
+	sw	ra,76(sp)
+	sw	s0,72(sp)
 	addi	s0,sp,80
 	sw	a0,-68(s0)
 	sw	a1,-72(s0)
@@ -32,10 +33,10 @@ main:
 	lw	a4,8(a4)
 	addi	a5,a5,%lo(.LC0)
 	lw	a5,12(a5)
-	sw	a2,-40(s0)
-	sw	a3,-36(s0)
-	sw	a4,-32(s0)
-	sw	a5,-28(s0)
+	sw	a2,-44(s0)
+	sw	a3,-40(s0)
+	sw	a4,-36(s0)
+	sw	a5,-32(s0)
 	lui	a5,%hi(.LC1)
 	lw	a2,%lo(.LC1)(a5)
 	addi	a4,a5,%lo(.LC1)
@@ -44,29 +45,35 @@ main:
 	lw	a4,8(a4)
 	addi	a5,a5,%lo(.LC1)
 	lw	a5,12(a5)
-	sw	a2,-56(s0)
-	sw	a3,-52(s0)
-	sw	a4,-48(s0)
-	sw	a5,-44(s0)
+	sw	a2,-60(s0)
+	sw	a3,-56(s0)
+	sw	a4,-52(s0)
+	sw	a5,-48(s0)
 	li	a5,2
 	sw	a5,-20(s0)
-	li	a5,9
+	li	a5,986
 	sw	a5,-24(s0)
-	lw	a5,-24(s0)
+	li	a5,9
+	sw	a5,-28(s0)
+	lw	a5,-28(s0)
  #APP
-# 12 "cvectest.c" 1
-	vsetvl a5, a5;
+# 13 "cvectest.c" 1
+	mv a5, a5;
 # 0 "" 2
  #NO_APP
 	sw	a5,-20(s0)
+	lw	a5,-24(s0)
+	lw	a4,-28(s0)
  #APP
-# 13 "cvectest.c" 1
-	vfence;
+# 14 "cvectest.c" 1
+	vsetvl a5, a5, a4;
 # 0 "" 2
  #NO_APP
+	sw	a5,-20(s0)
 	li	a5,0
 	mv	a0,a5
-	lw	s0,76(sp)
+	lw	ra,76(sp)
+	lw	s0,72(sp)
 	addi	sp,sp,80
 	jr	ra
 	.size	main, .-main
